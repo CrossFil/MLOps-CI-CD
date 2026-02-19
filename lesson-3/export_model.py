3946cc4e4307f335d1746b7ee7366cb7a361c3b4
Oleg Filonenko <166287636+CrossFil@users.noreply.github.com>
2026-02-19 09:04:11 +0100

    Update export_model.py

--- lesson-3/export_model.py
+++ lesson-3/export_model.py
@@ -1,14 +1,14 @@
 import torch
 import torchvision.models as models
 
-# Загружаем модель
+# load the model
 model = models.mobilenet_v2(weights='DEFAULT')
 model.eval()
 
-# Делаем трассировку (превращаем в TorchScript)
+# perform tracing (convert to TorchScript)
 example = torch.rand(1, 3, 224, 224)
 traced_script_module = torch.jit.trace(model, example)
 
-# Сохраняем в файл
+# save the file
 traced_script_module.save("model.pt")
-print("✅ Модель успешно сохранена как model.pt")
+print("✅ Model successfully saved as model.pt")
