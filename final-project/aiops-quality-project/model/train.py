32fe89e8577c9fda34d5188549938f53840a3ae0
Oleg Filonenko <166287636+CrossFil@users.noreply.github.com>
2026-03-15 10:26:07 +0100

    Update train.py

--- final-project/aiops-quality-project/model/train.py
+++ final-project/aiops-quality-project/model/train.py
@@ -1,1 +1,17 @@
 # retrain script
+import pickle
+import os
+
+def train_model():
+
+    model = {"version": 1.0, "coefficient": 0.5}
+    
+
+    os.makedirs('model', exist_ok=True)
+    
+    with open('model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    print("Model trained and saved to model/model.pkl")
+
+if __name__ == "__main__":
+    train_model()
